239e39705178c25859c156453437a1fc6363d75e
D09 <57136627+devashishpan@users.noreply.github.com>
2022-01-31 19:00:01 +0530

    Update Web_server_setup.tf

--- Web_server_setup.tf
+++ Web_server_setup.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "sg_web" {
   ingress {
     from_port   = 80
     to_port     = 80
-    protocol    = "http"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
@@ -22,7 +22,7 @@ resource "aws_security_group" "sg_web" {
 }
 
 resource "aws_instance" "web_server" {
-  ami                    = "ami-010aff33ed5991201"
+  ami                    = "ami-06cffe063efe892ad"
   instance_type          = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.sg_web.id}"]
   key_name               = var._keyname_
@@ -34,21 +34,21 @@ resource "aws_instance" "web_server" {
 resource "null_resource" "config_webserver" {
   connection {
     type        = "ssh"
-    user        = "root"
+    user        = "ec2-user"
     private_key = file("${var._key_file_}")
     host        = aws_instance.web_server.public_ip
   }
   provisioner "remote-exec" {
     inline = [
-      "sudo yum install httpd php -y",
+      "sudo yum install httpd -yy",
       "sudo systemctl enable httpd",
       "sudo systemctl start httpd",
       "sudo yum install git -y",
-      "sudo git clone ${var._git_url_} > /var/www/html"
+      "sudo git clone ${var._git_url_} > /var/www/html",
+      "sudo systemctl restart httpd"
     ]
   }
   provisioner "local-exec" {
     command = "google-chrome ${aws_instance.web_server.public_ip}"
   }
 }
-
