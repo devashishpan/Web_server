4e608be9f5c3a493b9196bcea1f01d929474b3bd
D09 <57136627+devashishpan@users.noreply.github.com>
2022-01-31 17:14:59 +0530

    Update Web_server_setup.tf

--- Web_server_setup.tf
+++ Web_server_setup.tf
@@ -1,14 +1,22 @@
+resource "aws_security_group" "sg_web" {
+	name = "sg_web"
+	ingress {
+		from_port   = "80"
+		to_port     = "80"
+		protocol    = "http"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+}
 
 resource "aws_instance" "web_server"{
 	ami = "ami-010aff33ed5991201"
 	instance_type = "t2.micro"
-	security_groups = ["web_server"]
+	vpc_security_group_ids = [aws_security_group.sg_web.id]
 	key_name = var._keyname_
 	tags = {
 		Name = "Webserver"
 	}
 }
-
 resource "null_resource" "config_webserver" {
 	connection {
 		type = "ssh"
