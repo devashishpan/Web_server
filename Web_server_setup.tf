59bac885af4069a943d61b590ca5029d539c5fe2
D09 <57136627+devashishpan@users.noreply.github.com>
2022-01-31 17:33:56 +0530

    Update Web_server_setup.tf

--- Web_server_setup.tf
+++ Web_server_setup.tf
@@ -24,7 +24,7 @@ resource "aws_security_group" "sg_web" {
 resource "aws_instance" "web_server"{
 	ami 			= "ami-010aff33ed5991201"
 	instance_type 		= "t2.micro"
-	vpc_security_group_ids 	= [aws_security_group.sg_web.id]
+	vpc_security_group_ids 	= ["${aws_security_group.sg_web.id}"]
 	key_name 		= "${var._keyname_}"
 	tags 			= {
 			Name 	= "Webserver"
